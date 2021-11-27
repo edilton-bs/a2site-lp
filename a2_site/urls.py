2afcce06dbf13c87abd2ea59615e44cecb5c743a
edilton-bs <ediltonbs5@gmail.com>
2021-11-27 19:42:33 -0300

    primeiro commit

---
+++ a2_site/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path(r'edilton/', include('edilton.urls')),
+]
