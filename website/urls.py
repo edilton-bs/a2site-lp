4e2e6ed395aa36dcc055511c65fc1bf5bcd5694d
Edilton Brandão <ediltonbs5@gmail.com>
2021-12-02 20:40:48 -0300

    importando conteúdo estático a1 completo

--- website/urls.py
+++ website/urls.py
@@ -4,5 +4,10 @@ from website import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('edit-profile', views.edit_profile, name='edit_profile'),
+    path('forum', views.forum, name='forum'),
+    path('nota', views.nota, name='nota'),
+    path('main', views.main, name='main'),
+    path('profile', views.profile, name='profile'),
 ]
 
--- website/views.py
+++ website/views.py
@@ -6,3 +6,36 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request,'index.html')
+
+
+def main(request):
+    """View function for home page of site."""
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request,'main.html')
+
+def nota(request):
+    """View function for home page of site."""
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request,'nota.html')
+
+
+def forum(request):
+    """View function for home page of site."""
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request,'forum.html')
+
+def edit_profile(request):
+    """View function for home page of site."""
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request,'edit-profile.html')
+
+
+def profile(request):
+    """View function for home page of site."""
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request,'profile.html')
