51d2023c01a7bef3a9e64a08f5a5b991b32159d1
Edilton Brandão <ediltonbs5@gmail.com>
2021-12-03 08:05:54 -0300

    criado modelo materias e página de admin

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from website.models import Materias
+
+admin.site.register(Materias)
 
 # Register your models here.
--- website/models.py
+++ website/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Materias(models.Model):
+    name = models.CharField(max_length=100)
+    url_image = models.CharField(max_length=300)
+    summary = models.TextField(max_length=1000)
