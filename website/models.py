779f901df81bd59a31650a3296a927045ea8ecfe
Edilton Brandão <ediltonbs5@gmail.com>
2021-12-03 08:34:59 -0300

    Update models.py

--- website/models.py
+++ website/models.py
@@ -4,3 +4,7 @@ class Materias(models.Model):
     name = models.CharField(max_length=100)
     url_image = models.CharField(max_length=300)
     summary = models.TextField(max_length=1000)
+    
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.title
